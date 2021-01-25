88838a4e08bde37d3bb43723c7d094e2be193113
Winston McGriff <winstonmcgriff27@gmail.com>
2021-01-25 21:39:30 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,4 +4,12 @@ else
   puts "Odd!"
 end 
 
+current_time = time.now 
+
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else puts "Odd!"
+end
 # Write your solution here
